964bc54dcf2c52456fc32381584a1a895b9883a9
André <andre.b.lindeloff@gmail.com>
2023-03-22 21:04:40 +0100

    random background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
+import type {Component} from 'solid-js';
 import styles from './App.module.css';
 import Countdown from "./countdown/Countdown";
+import {onMount} from "solid-js";
 
+const rosh = await import('./assets/rosh.jpg');
+const background = await import('./assets/background.jpg');
+const mauritiusIsland = await import('./assets/mauritius-island.jpg');
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-          <h1>Hvor lenge er det til Mauritius?</h1>
-      </header>
-      <main style={styles.mainContent}>
-        <Countdown></Countdown>
-      </main>
-    </div>
-  );
+
+    onMount(async () => {
+        randomImage();
+    });
+
+    const randomImage = () => {
+        const images = [rosh, background, mauritiusIsland];
+        const size = images.length;
+        const x = Math.floor(size * Math.random());
+        let selectedImage = images[x];
+        document.body.style.backgroundImage = "url(" + selectedImage.default + ")";
+    }
+
+
+    return (
+        <div class={styles.App}>
+            <header class={styles.header}>
+                <h1>Hvor lenge er det til Mauritius?</h1>
+            </header>
+            <main style={styles.mainContent}>
+                <Countdown></Countdown>
+            </main>
+        </div>
+    );
 };
 
 export default App;
