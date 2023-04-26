309e083a31936b205708f347c0c4de93ade8befa
André <andre.b.lindeloff@gmail.com>
2023-04-26 17:58:18 +0200

    added animation to bell

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,9 +1,24 @@
 import styles from './style.module.css';
+import {createEffect, createSignal} from "solid-js";
 
 function Footer() {
+    const [active, setActive] = createSignal(true);
+
+
+    createEffect(() => {
+        const interval = setInterval(() => {
+            setActive(!active());
+             console.log("active", active())
+        }, 5000);
+
+        return () => clearInterval(interval);
+    });
+
     return (
         <footer class={styles.footer}>
-            <a class={styles.wedding} href={"https://nadineoyvind.webnode.page/"}>👆Bryllup 👰🤵</a>
+            <a class={`${styles.wedding}`} href={"https://nadineoyvind.webnode.page/"}>
+                <span class={`${active() && styles.bell}`}>🔔</span>👆Bryllup 👰🤵
+            </a>
         </footer>
     );
 }
